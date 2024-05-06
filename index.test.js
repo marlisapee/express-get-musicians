a932078f6c0201074d29ea98e680c11f7463170f
marlisapee <mpee@tripadvisor.com>
2024-05-06 19:12:47 -0400

    created get route/test for musicians

--- index.test.js
+++ index.test.js
@@ -3,20 +3,15 @@ const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
 
-const request = require("supertest")
+const request = require('supertest');
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician } = require('./models/index');
 const app = require('./src/app');
-const seedMusician = require("./seedData");
-
+const seedMusician = require('./seedData');
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+  test('endpoint should return a 200 status code', async () => {
+    const response = await request(app).get('/musicians');
+    expect(response.statusCode).toBe(200);
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require('../models/index');
+const { db } = require('../db/connection');
 
 const port = 3000;
+const id = 1;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
 
+app.get('/musicians', async (req, res) => {
+  const musicians = await Musician.findAll();
+  res.json(musicians);
+});
 
-
-
-
-
+app.get(`/musicians/${id}`, async (req, res) => {
+  const musician = await Musician.findByPk(id);
+  res.json(musician);
+});
 
 module.exports = app;
